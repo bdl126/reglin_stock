52ac83ef55101e5f35f5b3db5f3f5c402f347f43
Bruno <bdelafontaine92@gmail.com>
2019-09-05 18:45:29 -0400

    Display works properly and accuracy is shown

--- app.py
+++ app.py
@@ -20,35 +20,5 @@ regression.clftype([2,3,4,5,6])
 regression.fit()
 regression.predict()
 regression.score()
-bestReg=regression.mostAccurate()
-
-print(bestReg)
-
-# clfreg = LinearRegression(n_jobs=-1)
-# clfreg.fit(X_train, Y_train)
-# dfreg = data.getRawExtract()
-# y_pred = clfreg.predict(X_test)
-#
-#
-#
-#
-# plotGraph(data.getRawExtract(),X_test,y_pred)
-#
-#
-# # Quadratic Regression 2
-# clfpoly2 = make_pipeline(PolynomialFeatures(2), Ridge())
-# clfpoly2.fit(X_train, Y_train)
-#
-# # Quadratic Regression 3
-# clfpoly3 = make_pipeline(PolynomialFeatures(3), Ridge())
-# clfpoly3.fit(X_train, Y_train)
-#
-# #
-# #
-# confidencereg = clfreg.score(X_test, Y_test)
-# confidencepoly2 = clfpoly2.score(X_test, Y_test)
-# confidencepoly3 = clfpoly3.score(X_test,Y_test)
-#
-# print(confidencereg)
-# print(confidencepoly2)
-# print(confidencepoly3)
+bestReg=regression.mostAccurateScore()
+regression.plotGraph(data.getRawExtract())
--- function/RegClf.py
+++ function/RegClf.py
@@ -1,8 +1,11 @@
 import sys
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression,Ridge,LassoLars
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
+from pylab import text
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import re
 sys.path.insert(0, "function/")
 from plot import *
 
@@ -18,10 +21,13 @@ class RegClf():
         self.clf = []
         self.Y_pred = []
         self.confidencereg  = []
+        self.accuracy= []
+        self.posBestClf=0;
 
 
     def clftype(self,degre_ask):
         self.clf.append(LinearRegression(n_jobs=-1))
+        self.clf.append(LassoLars(alpha=0.1))
         for degree in degre_ask:
             self.clf.append(make_pipeline(PolynomialFeatures(degree), Ridge()))
 
@@ -39,6 +45,7 @@ class RegClf():
          for regression in self.clf:
             self.confidencereg.append(regression.score(self.X_test, self.Y_test))
 
+
     def mostAccurateScore (self):
         most_reliable=0;
         for pos in range(0,len(self.confidencereg)):
@@ -53,5 +60,33 @@ class RegClf():
         for pos in range(0,len(self.confidencereg)):
             if self.confidencereg[pos] > self.confidencereg[most_reliable]:
                 most_reliable=pos
-
+        self.posBestClf=most_reliable
         return self.clf[most_reliable]
+
+
+    def plotGraph(self, dataset):
+
+        fig=figure(num=None, figsize=(20, 16), dpi=80, facecolor='w', edgecolor='k')
+        #figure.suptitle('Adj Close vs Date', fontsize=16)
+        fig.canvas.set_window_title('Adj Close vs Date')
+        dataset = dataset.reset_index()
+        for regression in range(0,len(self.clf)):
+            plt.subplot(3, 3, regression + 1)
+
+            plt.plot(dataset['Date'][-self.Y_pred[regression].size:], self.Y_pred[regression], color='red', linewidth=2, label='Prediction')
+            plt.scatter(dataset['Date'][-self.Y_pred[regression].size:], dataset['Adj Close'][-self.Y_pred[regression].size:], color='blue', s=2,
+                        label='Data samples')
+            plt.text('2018-09', 220, 'accuracy:'+str(self.confidencereg[regression]),backgroundcolor="#D3D3D3")
+            plt.legend()
+
+            #if it's polynomyal, get it's degree
+            if hasattr(self.clf[regression], 'steps'):
+                plt.title('polynomial ' + str(self.clf[regression].steps[0][1].degree))
+            else:
+
+                title=self.clf[regression].__class__.__name__
+                plt.title(title)
+
+            plt.ylabel('Adj Close')
+            plt.xlabel('Date')
+        plt.show()
